bfb2556266a892095a69c1fb6035cd125ac7a171
gabrielqaddaha <gabriel.qaddaha@ecole-hexagone.com>
2024-04-17 14:08:48 +0200

    modification

--- COURS_2/KOINKOIN.py
+++ COURS_2/KOINKOIN.py
@@ -1,81 +1,83 @@
 from abc import ABC, abstractmethod
 
-class FlyBehavior(ABC) :
+
+class FlyBehavior(ABC):
 
     @abstractmethod
-    def voler(self) :
+    def voler(self):
         pass
 
-class Flyable(FlyBehavior) :
 
+class Flyable(FlyBehavior):
     def voler(self) -> None:
+
         print("I really can fly !!!")
 
-class NotFlyable(FlyBehavior) :
 
+class NotFlyable(FlyBehavior):
     def voler(self) -> None:
         print("I believe i can fly but not realy ...")
 
-class QuackBehavior(ABC) :
-    
-        @abstractmethod
-        def cancaner(self) :
-            pass
 
-class CanCan(QuackBehavior) :
-    
-        def cancaner(self) -> None:
-            print("\nCoin Coin")
+class QuackBehavior(ABC):
+    @abstractmethod
+    def cancaner(self):
+        pass
 
-class NotCanCan(QuackBehavior) :
-        
-            def cancaner(self) -> None:
-                print("\nI can't COIN COIN !!!!!!!!!!!!!!! ")
 
-class Canard(ABC) :
+class CanCan(QuackBehavior):
+    def cancaner(self) -> None:
+        print("\nCoin Coin")
 
-    def __init__(self, name = 'undefined', flyBehavior = None, quackBehavior = None):
+
+class NotCanCan(QuackBehavior):
+
+    def cancaner(self) -> None:
+        print("\nI can't COIN COIN !!!!!!!!!!!!!!! ")
+
+
+class Canard(ABC):
+
+    def __init__(self, name="undefined", flyBehavior=None, quackBehavior=None):
         self.__name = name
         self.__flyBehavior = flyBehavior
         self.__quackBehavior = quackBehavior
 
     @property
-    def name(self) :
+    def name(self):
         return self.__name
-    
+
     @property
-    def flyBehavior(self) -> FlyBehavior :
+    def flyBehavior(self) -> FlyBehavior:
         return self.__flyBehavior
-    
+
     @property
-    def quackBehavior(self) -> QuackBehavior :
+    def quackBehavior(self) -> QuackBehavior:
         return self.__quackBehavior
-    
-    def voler(self) :
+
+    def voler(self):
         self.flyBehavior.voler()
-    
-    def cancaner(self) :
+
+    def cancaner(self):
         self.quackBehavior.cancaner()
 
-    
-class Colvert(Canard) :
+
+class Colvert(Canard):
 
     def __init__(self, name) -> None:
         super().__init__(name, Flyable(), CanCan())
 
 
-class CouRouge(Canard) :
+class CouRouge(Canard):
 
     def __init__(self, name) -> None:
         super().__init__(name, NotFlyable(), NotCanCan())
 
 
-
-if __name__ == '__main__' :
+if __name__ == "__main__":
     colvert = Colvert("coucou")
     colvert.voler()
     colvert.cancaner()
     couRouge = CouRouge("zoubi")
     couRouge.voler()
     couRouge.cancaner()
-
